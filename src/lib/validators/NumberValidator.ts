9b94b5949bb73d8ff54775f4e9cf2ce3bf56ab60
Peter Kottas <petokottas@gmail.com>
2017-12-06 15:26:17 +0100

    Added min max to number validator

--- src/lib/validators/NumberValidator.ts
+++ src/lib/validators/NumberValidator.ts
@@ -1,10 +1,24 @@
 import { IBaseValidator } from './base';
+export interface NumberValidatorConfig {
+    min: number,
+    max: number
+}
 
 export class NumberValidator implements IBaseValidator {
+    private config: NumberValidatorConfig;
+    constructor(config: NumberValidatorConfig = { min: undefined, max: undefined }) {
+        this.config = config;
+    }
     public static instance = new NumberValidator();
     public Validate(value: string, isRequired: boolean, addError: (error: string) => void): boolean {
         let number = Number(value);
         if (!isNaN(number)) {
+            if (this.config.min && number <= this.config.min) {
+                return false;
+            }
+            if (this.config.max && number >= this.config.max) {
+                return false;
+            }
             return true;
         }
         addError('Invalid number');
