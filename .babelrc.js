e38d64a5797ca4d0778a23b1801dad46086d7c2f
Peter Kottas <petokottas@gmail.com>
2021-08-10 18:34:30 +0200

    Deploy commit

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['@babel/env', '@babel/typescript', '@babel/preset-react'],
   plugins: [
+    '@babel/plugin-proposal-class-static-block',
     '@babel/proposal-class-properties',
     '@babel/proposal-object-rest-spread',
     '@babel/plugin-proposal-optional-chaining',
